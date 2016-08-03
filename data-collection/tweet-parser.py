7b0685510188822b13f5668a37b96d7225582b45
ahendy0 <ahendy@hotmail.ca>
2016-08-02 23:04:49 -0700

    Add tweet retrieval and write to file (NO EMOJI :/)

--- data-collection/tweet-parser.py
+++ data-collection/tweet-parser.py
@@ -1,8 +1,27 @@
+import twitter
+
 from config import (
 	TWITTER_CONSUMER_KEY,
 	TWITTER_CONSUMER_SECRET,
 	TWITTER_ACCESS_TOKEN_KEY,
 	TWITTER_ACCESS_TOKEN_SECRET,)
 
-if __name__ == '__main__':
-	print "Hello World"
+api = twitter.Api(
+		consumer_key=TWITTER_CONSUMER_KEY,
+		consumer_secret=TWITTER_CONSUMER_SECRET,
+		access_token_key=TWITTER_ACCESS_TOKEN_KEY,
+		access_token_secret=TWITTER_ACCESS_TOKEN_SECRET)
+
+KANYE = '@kanyewest'
+MAX_TWEETS = 200
+
+statuses = api.GetUserTimeline(screen_name=KANYE, count=MAX_TWEETS)
+
+with open('kanye.txt', 'w') as f:
+	"""Use 'a' for append"""
+	for status in statuses:
+		try:
+			f.write(status.text + '\n')
+		except UnicodeEncodeError as e:
+			unicode_str = status.text.encode('ascii', 'ignore')
+			f.write(unicode_str + '\n')
