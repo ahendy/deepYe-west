7be058530b196327bf6248a70cba67fcaa384a9b
ahendy0 <ahendy@hotmail.ca>
2016-08-03 22:53:14 -0700

    Retrieval of ALL tweets --note, limited API calls to 15

--- data-collection/lyric-parser.py
+++ data-collection/lyric-parser.py
@@ -17,25 +17,23 @@ Now call GetLyric api with
 GetLyric?lyricId=<LyricID>&lyricCheckSum=<LyricCheckSum>
 """
 with open(song_list_path, 'r') as f:
-	
-	lines = f.readlines()
-	for title in lines:	
-		
-		lyric_url = base_url % ("SearchLyric")  
-		params = {
-			'artist': 'Kanye West',
-			'song': title
-		}
-		response = requests.get(lyric_url, params)
-		print response.status_code
 
-		if response.status_code == 200:
-			
-			song_xml = response.text
-			e = ET.fromstring(song_xml)
-			for serach_result in e.findall('SerachLyricResult'):
-				
-				print serach_result.attrib
-		
-		else:
-			print 'API error:', response.text
+    lines = f.readlines()
+    for title in lines:
+
+        lyric_url = base_url % ("SearchLyricDirect")
+        params = {
+            'artist': 'Kanye West',
+            'song': title
+        }
+        response = requests.get(lyric_url, params)
+        print response.status_code
+
+        if response.status_code == 200:
+
+            song_xml = response.text
+            e = ET.fromstring(song_xml)
+            lyrics = e[-1].text
+
+        else:
+            print 'API error:', response.text
--- data-collection/tweet-parser.py
+++ data-collection/tweet-parser.py
@@ -1,27 +1,60 @@
 import twitter
+from twitter import TwitterError
 
 from config import (
-	TWITTER_CONSUMER_KEY,
-	TWITTER_CONSUMER_SECRET,
-	TWITTER_ACCESS_TOKEN_KEY,
-	TWITTER_ACCESS_TOKEN_SECRET,)
+    TWITTER_CONSUMER_KEY,
+    TWITTER_CONSUMER_SECRET,
+    TWITTER_ACCESS_TOKEN_KEY,
+    TWITTER_ACCESS_TOKEN_SECRET,)
 
 api = twitter.Api(
-		consumer_key=TWITTER_CONSUMER_KEY,
-		consumer_secret=TWITTER_CONSUMER_SECRET,
-		access_token_key=TWITTER_ACCESS_TOKEN_KEY,
-		access_token_secret=TWITTER_ACCESS_TOKEN_SECRET)
+        consumer_key=TWITTER_CONSUMER_KEY,
+        consumer_secret=TWITTER_CONSUMER_SECRET,
+        access_token_key=TWITTER_ACCESS_TOKEN_KEY,
+        access_token_secret=TWITTER_ACCESS_TOKEN_SECRET)
 
 KANYE = '@kanyewest'
-MAX_TWEETS = 200
+MAX_TWEETS = 1000
+fname = 'kanye'
+file = '.'.join([fname, 'txt'])
 
-statuses = api.GetUserTimeline(screen_name=KANYE, count=MAX_TWEETS)
+with open(file, 'w') as f:
+    """Use 'a' for append"""
+    tweet_count = 0
+    MAX_TWEETS = 0
+    # Max id is a parameter which returns the data only previous to it
+    MAX_ID = None
 
-with open('kanye.txt', 'w') as f:
-	"""Use 'a' for append"""
-	for status in statuses:
-		try:
-			f.write(status.text + '\n')
-		except UnicodeEncodeError as e:
-			unicode_str = status.text.encode('ascii', 'ignore')
-			f.write(unicode_str + '\n')
+    try:
+        while True:
+            statuses = api.GetUserTimeline(screen_name=KANYE, max_id=MAX_ID)
+            num_tweets = len(statuses)
+            MAX_TWEETS += num_tweets
+
+            if num_tweets == 0:
+                # No more tweets
+                break
+            
+            MAX_ID = min(statuses, key=lambda x: x.id).id
+            
+            for status in statuses:
+                
+                try:
+                    f.write(status.text + '\n')
+
+                except UnicodeEncodeError as e:
+                    # Ignore ascii for now
+                    unicode_str = status.text.encode('ascii', 'ignore')
+                    f.write(unicode_str + '\n')
+                
+                else:
+                    tweet_count += 1 
+
+            print "%d/%d tweets written" % (tweet_count, MAX_TWEETS)
+    
+    except TwitterError as e:
+        
+        print e
+        print "Possibly no more api requests"
+
+print file, "finished writing"
